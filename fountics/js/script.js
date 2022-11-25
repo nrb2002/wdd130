348610e4fab59d20b78d6db4d11371395f48bfbf
nrb2002 <tshibasubaron@yahoo.fr>
2022-11-25 19:07:56 +0100

    New update

---
+++ fountics/js/script.js
@@ -0,0 +1,7 @@
+/* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+} )
