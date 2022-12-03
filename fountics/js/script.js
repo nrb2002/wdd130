6dec1c02778fe975880f6d8e275ee74f12369dcd
nrb2002 <tshibasubaron@yahoo.fr>
2022-12-03 17:17:16 +0100

    Create a component

--- fountics/js/script.js
+++ fountics/js/script.js
@@ -1,7 +1,21 @@
 /* Toggle between adding and removing the "responsive" class to navbar when the user clicks on the icon */
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const openButton = document.getElementsByClassName('open')[0]
+const closeButton = document.getElementsByClassName('close')[0]
+
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    openButton.classList.toggle('active')
 } )
+
+
+$(document).ready(function(){
+    $(window).bind('scroll', function(){
+        var gap = 80;
+        if($(window).scrollTop > gap){
+            $('header').addClass('active');
+        }else{
+            $('header').removeClass('active');
+        }
+    });
+});
